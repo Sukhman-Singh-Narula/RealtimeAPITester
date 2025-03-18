74db8f6d5a688bb3ed3aa8ae71f7d9129d89b2cc
Sukhman-Singh-Narula <sukhmannarula84@gmail.com>
2025-03-18 16:12:48 +0530

    intial commit working

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { join, dirname } from "path";
+import { join, dirname, resolve } from "path";
 import { fileURLToPath } from "url";
+import react from "@vitejs/plugin-react";
 
 const path = fileURLToPath(import.meta.url);
 
-export default defineConfig({
+export default {
   root: join(dirname(path), "client"),
   plugins: [react()],
-  build: {
-    outDir: "../dist/client", // Ensure output is correct
-    emptyOutDir: true,
-    ssrManifest: true, // Needed for SSR
-    rollupOptions: {
-      input: "index.html",
-    },
-  },
-  server: {
-    port: 3000,
-  },
-});
+};
